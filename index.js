65557e262280585bd72a4926142579eff318c54c
Mariel Suing <marielsuing18@gmail.com>
2024-03-29 21:49:35 +0800

    Finalization of Details

--- index.js
+++ index.js
@@ -5,60 +5,67 @@ const key = document.querySelector('#key');
 const listLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
                     'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-let newText = '';
 
+function keyValidation(event) {
+    let shiftInput = event.target.value;
+    shiftInput = shiftInput.replace(/[^a-zA-Z]/g, '');
+    event.target.value = shiftInput;
+}
+                    
 function encryptText() {
-    btnEncrypt.addEventListener('click', () => {
-        const textarea = document.querySelector('#text');
-        const keyValue = Number(key.value);
+    const textarea = document.querySelector('#text');
+    const resultArea = document.querySelector('#result');
+    const keyValue = Number(key.value);
+    let newText = '';
 
-        for (let letter of textarea.value) {
-            letter = letter.toLowerCase();
-            if(!listLetters.includes(letter)) {
-                continue
-            }
-
-            const indexOfLetter = listLetters.findIndex((item) => item === letter);
-            let indexNewLetter = indexOfLetter + keyValue;
-
-            if(indexNewLetter > 25) {
-                indexNewLetter -= 26;
-            }
-
-            newText += listLetters[indexNewLetter];
-            textarea.value = newText;
+    for (let letter of textarea.value) {
+        letter = letter.toLowerCase();
+        if (!listLetters.includes(letter)) {
+            newText += letter;
+            continue;
         }
-        newText = '';
-    });
+
+        const indexOfLetter = listLetters.findIndex((item) => item === letter);
+        let indexNewLetter = indexOfLetter + keyValue;
+
+        if (indexNewLetter > 25) {
+            indexNewLetter -= 26;
+        }
+
+        newText += listLetters[indexNewLetter];
+    }
+
+    resultArea.value = newText;
 };
 
 function decryptText() {
-    btnDecrypt.addEventListener('click', () => {
-        const textarea = document.querySelector('#text');
-        const keyValue = Number(key.value);
+    const textarea = document.querySelector('#text');
+    const resultArea = document.querySelector('#result');
+    const keyValue = Number(key.value);
+    let newText = '';
 
-        for (let letter of textarea.value) {
-            letter = letter.toLowerCase();
-            if(!listLetters.includes(letter)) {
-                continue
-            }
-
-            const indexOfLetter = listLetters.findIndex((item) => item === letter);
-            let indexNewLetter = indexOfLetter - keyValue;
-
-            if(indexNewLetter < 0) {
-                indexNewLetter += 26;
-            }
-
-            newText += listLetters[indexNewLetter];
-            textarea.value = newText;
+    for (let letter of textarea.value) {
+        letter = letter.toLowerCase();
+        if (!listLetters.includes(letter)) {
+            newText += letter;
+            continue;
         }
-        newText = '';
-    });
+
+        const indexOfLetter = listLetters.findIndex((item) => item === letter);
+        let indexNewLetter = indexOfLetter - keyValue;
+
+        if (indexNewLetter < 0) {
+            indexNewLetter += 26;
+        }
+
+        newText += listLetters[indexNewLetter];
+    }
+
+    resultArea.value = newText;
 };
 
 function clearTextArea() {
-    btnClear.addEventListener('click', () => {
-        document.getElementById('text').value='';
-    });
+    document.getElementById('text').value = '';
+    document.getElementById('key').value = '';
+    document.getElementById('result').value = '';
 };
